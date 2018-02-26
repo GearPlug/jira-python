0bf71e52202aaf27dbf1311e3ab398ff156f3c97
Miguel Ferrer <ingferrermiguel@gmail.com>
2018-02-26 08:35:18 -0500

    Add MANIFEST.in; Bump ver. 0.2.1

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import os
 from setuptools import setup
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 setup(name='jira-python',
-      version='0.2',
+      version='0.2.1',
       description='API wrapper for Jira CRM written in Python',
+      long_description=read('README.md'),
       url='https://github.com/GearPlug/jira-python',
       author='Miguel Ferrer',
       author_email='ingferrermiguel@gmail.com',
